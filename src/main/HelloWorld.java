7e58697a266b1f28be5f215470ef30c7ea87b17e
hwc <huwencheng@aris.org.cn>
2015-09-24 17:50:10 +0800

    java更新

---
+++ src/main/HelloWorld.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package main;
+/** 
+ * @proje_name：test 
+ * @class_name：HelloWorld 
+ * @class_desc： 
+ * @owner_name：huwencheng
+ * @company：www.aris.org.cn
+ * @contact：huwencheng@aris.org.cn
+ * @datetim：2015年9月24日  下午5:46:54
+ * @version
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Aris.");
+	}
+
+}
